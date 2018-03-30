39f31e0cce67e9e4ca4139be17c4c6071adf2f9c
cotttpan <cotttpan@gmail.com>
2018-03-30 21:55:31 +0900

    refactor: refactor using PatchTtarget type

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { Reducer, AnyAction } from 'redux'
-import { Command, CommandCreator, EmptyCommandCreator } from 'command-bus'
+import { Command, AnyCommandCreator } from 'command-bus'
 
-export type AnyCommandCreator<T = undefined> = CommandCreator<T> | EmptyCommandCreator
 export type Patch<S, A> = (state: S, aciton: Command<A>) => S
-export interface ReducerMap<S> { [key: string]: Reducer<S> }
+export type ReducerMap<S> = { [key: string]: Reducer<S> }
+export type PatchTarget<T> = AnyCommandCreator<T> | AnyCommandCreator<T>[]
 export type InitialStateFactory<S> = () => S
 
 function ensureArray<T>(src: T | T[]) {
   return Array.isArray(src) ? src : [src]
 }
 
-export function caseOf<S, A>(commandCreator: AnyCommandCreator<A> | AnyCommandCreator<A>[], patch: Patch<S, A>) {
+export function caseOf<S, A>(target: PatchTarget<A>, patch: Patch<S, A>) {
   const reducerMap: ReducerMap<S> = {}
-  for (const creator of ensureArray(commandCreator)) {
+  for (const creator of ensureArray(target)) {
     reducerMap[creator.type] = patch as Reducer<S>
   }
   return reducerMap
@@ -21,7 +21,7 @@ export function caseOf<S, A>(commandCreator: AnyCommandCreator<A> | AnyCommandCr
 export function createReducer<S>(init: InitialStateFactory<S>) {
   return (...patchMaps: ReducerMap<S>[]): Reducer<S> => {
     const initialState = init()
-    const patchMap: ReducerMap<S> = Object.assign.call(null, {}, ...patchMaps)
+    const patchMap: ReducerMap<S> = Object.assign({}, ...patchMaps)
     return (state = initialState, action: AnyAction) => {
       const patch = patchMap[action.type]
       return patch ? patch(state, action) : state
