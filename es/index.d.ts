abc0d386635ac5103284149d17cdb992696af477
cotttpan <cotttpan@gmail.com>
2018-04-19 16:35:39 +0900

    0.0.3

--- es/index.d.ts
+++ es/index.d.ts
@@ -1,4 +1,4 @@
-import { Reducer } from 'redux';
+import { Reducer, AnyAction } from 'redux';
 import { Command, AnyCommandCreator } from 'command-bus';
 export declare type Patch<S, A> = (state: S, aciton: Command<A>) => S;
 export declare type ReducerMap<S> = {
@@ -7,4 +7,4 @@ export declare type ReducerMap<S> = {
 export declare type PatchTarget<T> = AnyCommandCreator<T> | AnyCommandCreator<T>[];
 export declare type InitialStateFactory<S> = () => S;
 export declare function caseOf<S, A>(target: PatchTarget<A>, patch: Patch<S, A>): ReducerMap<S>;
-export declare function createReducer<S>(init: InitialStateFactory<S>): (...patchMaps: ReducerMap<S>[]) => Reducer<S>;
+export declare function createReducer<S>(init: InitialStateFactory<S>): (...patchMaps: ReducerMap<S>[]) => Reducer<S, AnyAction>;
