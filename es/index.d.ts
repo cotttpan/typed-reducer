ec0dd105489be51166ac96db50cf7ce7ade883b4
nullabletypo <masahirokato.dev@gmail.com>
2018-06-22 22:25:17 +0900

    0.1.3

--- es/index.d.ts
+++ es/index.d.ts
@@ -1,9 +1,11 @@
 import { Reducer, AnyAction } from 'redux';
-import { Command, AnyCommandCreator } from 'command-bus';
-export declare type Patch<S, A> = (state: S, action: Command<A>) => S;
+import { AnyCommand, AnyCommandCreator } from 'command-bus';
+export declare type ReturnTypeOnArrayFuncs<T> = T extends ((...val: any[]) => infer R)[] ? R : never;
+export declare type PatchFunc<S, T extends AnyCommand> = (state: S, command: T) => S;
 export declare type ReducerMap<S> = {
     [key: string]: Reducer<S>;
 };
-export declare type PatchTarget<T> = AnyCommandCreator<T> | AnyCommandCreator<T>[];
-export declare function caseOf<S, A>(target: PatchTarget<A>, patch: Patch<S, A>): ReducerMap<S>;
+export declare type PatchTarget = AnyCommandCreator | AnyCommandCreator[];
+export declare type PatchSource<T> = T extends AnyCommandCreator ? ReturnType<T> : T extends AnyCommandCreator[] ? ReturnTypeOnArrayFuncs<T> : never;
+export declare function caseOf<S, T extends PatchTarget>(target: T, patch: PatchFunc<S, PatchSource<T>>): ReducerMap<S>;
 export declare function createReducer<S>(init: () => S): (...maps: ReducerMap<S>[]) => Reducer<S, AnyAction>;
