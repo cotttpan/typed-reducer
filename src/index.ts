5692f5d9be5635d3c35a3ca36db41ac35b95dbb6
nullabletypo <masahirokato.dev@gmail.com>
2018-08-03 13:55:19 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,39 @@ import { Command, AnyCommandCreator } from 'command-bus'
 
 export type ReturnTypeOnArrayFuncs<T> = T extends ((...val: any[]) => infer R)[] ? R : never
 
-export type PatchFunc<S, T extends Command> = (state: S, command: T) => S
+export type Patch<S, T extends Command> = (state: S, command: T) => S
 
 export type ReducerMap<S> = { [key: string]: Reducer<S> }
 
-export type PatchTarget = AnyCommandCreator | AnyCommandCreator[]
+export type Target = AnyCommandCreator | AnyCommandCreator[] | string
 
-export type PatchSource<T> =
+export type PSource<T> =
   T extends AnyCommandCreator ? ReturnType<T> :
   T extends AnyCommandCreator[] ? ReturnTypeOnArrayFuncs<T> :
+  T extends string ? any :
   never
 
 function ensureArray<T>(src: T | T[]) {
   return Array.isArray(src) ? src : [src]
 }
 
-export function caseOf<S, T extends PatchTarget>(target: T, patch: PatchFunc<S, PatchSource<T>>) {
+function getType(src: string | AnyCommandCreator) {
+  return typeof src === 'string' ? src : src.type
+}
+
+export function caseOf<S, T extends Target>(target: T, patch: Patch<S, PSource<T>>) {
   const reducerMap: ReducerMap<S> = {}
   for (const creator of ensureArray(target)) {
-    reducerMap[(creator as any).type] = patch as Reducer<S>
+    reducerMap[getType(creator as any)] = patch as Reducer<S>
   }
   return reducerMap
 }
 
-export function createReducer<S>(init: () => S) {
+export function createReducer<S>(init: (() => S) | S) {
   return (...maps: ReducerMap<S>[]): Reducer<S> => {
     const map: ReducerMap<S> = Object.assign({}, ...maps)
-    return (state = init(), action: AnyAction) => {
+    const i = typeof init === 'function' ? init : () => init
+    return (state = i(), action: AnyAction) => {
       const patch = map[action.type]
       return patch ? patch(state, action) : state
     }
